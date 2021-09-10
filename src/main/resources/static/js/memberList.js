3b4c6f4f2fed33e84983c610a2dd0ff6be5929d4
Guitenbay <gtenbay@163.com>
2021-09-10 14:53:06 +0800

    feat: 优化依赖版本 && 修改实验室组别

--- src/main/resources/static/js/memberList.js
+++ src/main/resources/static/js/memberList.js
@@ -9,103 +9,61 @@ $.ajax({
         console.log( xhr );
     },
     success:function (d) {
-        var textIntel = "";
-        var textMobile = "";
-        var textBigData = "";
-        var textSoft = "";
-        var textClone = "";
-        var textCode = "";
-        var textDesign = "";
-        var textPP = "";
-        for(var i in d)
+        let textIntel = "";
+        let textBigData = "";
+        let textSoft = "";
+        let textDesignAndClone = "";
+        let textCode = "";
+        let textPP = "";
+        let textRobot = "";
+        let textAI = "";
+        let textTech = "";
+        for(let i in d)
         {
-            var degree;
+            let degree;
             if(d[i].type==="硕士") degree="硕士";
             else if(d[i].type==="博士") degree="博士";
 
-            var memberPhoto;
+            let memberPhoto;
             if(d[i].isphoto==="1")  memberPhoto=d[i].picture;
             else if(d[i].isphoto==="0") memberPhoto="default";
 
-            if(d[i].research==="云计算与微服务")
-                textIntel += "             <div class=\"AMember  col-md-3\">\n" +
-                    "                <img src=\"img/Member/"+ memberPhoto +".jpg\" alt=\"照片\" class=\"MemberPhoto\">\n" +
-                    "                <p>" + d[i].name + "</p>\n" +
-                    "                <p>"+ d[i].grade +"级"+ degree +"生</p>\n" +
-                    "            </div>" +
-                    ""
-            else if(d[i].research==="代码深度学习")
-                textMobile += "             <div class=\"AMember  col-md-3\">\n" +
-                    "                <img src=\"img/Member/"+ memberPhoto +".jpg\" alt=\"照片\" class=\"MemberPhoto\">\n" +
-                    "                <p>" + d[i].name + "</p>\n" +
-                    "                <p>"+ d[i].grade +"级"+ degree +"生</p>\n" +
-                    "            </div>" +
-                    ""
-            else if(d[i].research==="代码知识图谱")
-                textBigData += "             <div class=\"AMember  col-md-3\">\n" +
-                    "                <img src=\"img/Member/"+ memberPhoto +".jpg\" alt=\"照片\" class=\"MemberPhoto\">\n" +
-                    "                <p>" + d[i].name + "</p>\n" +
-                    "                <p>"+ d[i].grade +"级"+ degree +"生</p>\n" +
-                    "            </div>" +
-                    ""
-            else if(d[i].research==="软件分析与挖掘")
-                textSoft += "             <div class=\"AMember  col-md-3\">\n" +
-                    "                <img src=\"img/Member/"+ memberPhoto +".jpg\" alt=\"照片\" class=\"MemberPhoto\">\n" +
-                    "                <p>" + d[i].name + "</p>\n" +
-                    "                <p>"+ d[i].grade +"级"+ degree +"生</p>\n" +
-                    "            </div>" +
-                    ""
-            else if(d[i].research==="代码克隆分析")
-                textClone += "             <div class=\"AMember  col-md-3\">\n" +
-                    "                <img src=\"img/Member/"+ memberPhoto +".jpg\" alt=\"照片\" class=\"MemberPhoto\">\n" +
-                    "                <p>" + d[i].name + "</p>\n" +
-                    "                <p>"+ d[i].grade +"级"+ degree +"生</p>\n" +
-                    "            </div>" +
-                    ""
-            else if(d[i].research==="代码大数据平台")
-                textCode += "             <div class=\"AMember  col-md-3\">\n" +
-                    "                <img src=\"img/Member/"+ memberPhoto +".jpg\" alt=\"照片\" class=\"MemberPhoto\">\n" +
-                    "                <p>" + d[i].name + "</p>\n" +
-                    "                <p>"+ d[i].grade +"级"+ degree +"生</p>\n" +
-                    "            </div>" +
-                    ""
-            else if(d[i].research==="软件设计")
-                textDesign += "             <div class=\"AMember  col-md-3\">\n" +
-                    "                <img src=\"img/Member/"+ memberPhoto +".jpg\" alt=\"照片\" class=\"MemberPhoto\">\n" +
-                    "                <p>" + d[i].name + "</p>\n" +
-                    "                <p>"+ d[i].grade +"级"+ degree +"生</p>\n" +
-                    "            </div>" +
-                    ""
-            else if(d[i].research==="人机物融合软件技术")
-                textPP += "             <div class=\"AMember  col-md-3\">\n" +
-                    "                <img src=\"img/Member/"+ memberPhoto +".jpg\" alt=\"照片\" class=\"MemberPhoto\">\n" +
-                    "                <p>" + d[i].name + "</p>\n" +
-                    "                <p>"+ d[i].grade +"级"+ degree +"生</p>\n" +
-                    "            </div>" +
-                    ""
+            const tmpMember = "             <div class=\"AMember  col-md-3\">\n" +
+                "                <img src=\"img/Member/"+ memberPhoto +".jpg\" alt=\"照片\" class=\"MemberPhoto\">\n" +
+                "                <p>" + d[i].name + "</p>\n" +
+                "                <p>"+ d[i].grade +"级"+ degree +"生</p>\n" +
+                "            </div>" +
+                "";
 
-
+            if(d[i].research==="云原生与智能化运维组")
+                textIntel += tmpMember;
+            else if(d[i].research==="智能化软件开发组")
+                textBigData += tmpMember;
+            else if(d[i].research==="代码分析与优化组")
+                textSoft += tmpMember;
+            else if(d[i].research==="软件设计与克隆分析组")
+                textDesignAndClone += tmpMember;
+            else if(d[i].research==="代码大数据分析平台组")
+                textCode += tmpMember;
+            else if(d[i].research==="人机物融合智能组")
+                textPP += tmpMember;
+            else if(d[i].research==="机器人软件工程组")
+                textRobot += tmpMember;
+            else if(d[i].research==="AI系统工程组")
+                textAI += tmpMember;
+            else if(d[i].research==="AI系统工程组")
+                textTech += tmpMember;
         }
 
-
-
         $(".inteliInfo").html(textIntel);
-        $(".mobileInfo").html(textMobile);
         $(".bigDataInfo").html(textBigData);
         $(".softInfo").html(textSoft);
-        $(".cloneInfo").html(textClone);
+        $(".designAndCloneInfo").html(textDesignAndClone);
         $(".codeInfo").html(textCode);
-        $(".designInfo").html(textDesign);
         $(".ppInfo").html(textPP);
-        // $("div.holder").jPages({
-        //     containerID : "itemContainer",
-        //     previous : "←",
-        //     next : "→",
-        //     perPage : 5,
-        //     delay : 50
-        // });
-
-        // $("div.holder > a").css("font-size","20px")
+        $(".robotInfo").html(textRobot);
+        $(".aiInfo").html(textAI);
+        $(".techInfo").html(textTech);
     }
 
 })
